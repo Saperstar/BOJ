2092293a834a6c2422c1c3492e0972e1fa225156
Saperstar <112953455+Saperstar@users.noreply.github.com>
2026-03-06 02:47:54 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 116 ms, Memory: 15252 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		BigInteger one = BigInteger.ONE;
+		BigInteger two = BigInteger.TWO;
+		BigInteger[] fibo = new BigInteger[1000];
+		fibo[0] = one;
+		fibo[1] = one.add(two);
+		for(int i = 2; i < 1000; i++) {
+			fibo[i] = fibo[i-2].add(fibo[i-2]).add(fibo[i-1]);
+		}
+		int n = Integer.parseInt(br.readLine());
+		BigInteger slash = BigInteger.valueOf(10007);
+		bw.write(fibo[n-1].mod(slash) + "");
+		bw.flush();
+		
+		
+
+	}
+
+}
