40178745575a19d27fcd08980db9b87f83f97c6a
Saperstar <112953455+Saperstar@users.noreply.github.com>
2026-03-06 02:23:24 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 800 ms, Memory: 61392 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		int[][] points = new int[N][2];
+		for(int i = 0; i < N; i++) {
+			String line = br.readLine();
+			String[] lines = line.split(" ");
+			points[i][0] = Integer.parseInt(lines[0]);
+			points[i][1] = Integer.parseInt(lines[1]);
+			
+		}
+		
+		Arrays.sort(points, (a, b) -> {
+			int x1 = a[0];
+			int x2 = b[0];
+			int y1 = a[1];
+			int y2 = b[1];
+			if(y1 != y2) {
+				return y1 - y2; 
+			}
+			else {
+				return x1 - x2;
+			}
+		});
+		
+		for(int i = 0; i < N; i++) {
+			bw.write(points[i][0] + " " + points[i][1] + "\n");
+		}
+		
+		bw.flush();
+		
+
+	}
+
+}
+	
