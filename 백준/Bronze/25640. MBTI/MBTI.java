da34506d560b7cad840ffa8af27da0277ffb328a
Saperstar <112953455+Saperstar@users.noreply.github.com>
2026-03-06 02:26:14 +0900

    [Bronze IV] Title: MBTI, Time: 116 ms, Memory: 15828 KB -BaekjoonHub

---
+++ 백준/Bronze/25640. MBTI/MBTI.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String myM = br.readLine();
+		int N = Integer.parseInt(br.readLine());
+		int ans = 0;
+		for(int i = 0; i < N; i++) {
+			String friM = br.readLine();
+			if(myM.equals(friM)) ans++;
+		}
+		
+		bw.write(ans + "\n");
+		bw.flush();
+		
+		
+
+	}
+	
+
+}
