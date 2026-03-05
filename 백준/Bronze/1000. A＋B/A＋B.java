9c3b7541a613b3b40066d2dff2e391e5df1ed3f4
Saperstar <112953455+Saperstar@users.noreply.github.com>
2026-03-06 01:26:34 +0900

    [Bronze V] Title: A+B, Time: 176 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    int a;
+	    int b;
+	    
+	    Scanner sc = new Scanner(System.in);
+		a = sc.nextInt();
+		b = sc.nextInt();
+		System.out.println(a+b);
+	}
+}
