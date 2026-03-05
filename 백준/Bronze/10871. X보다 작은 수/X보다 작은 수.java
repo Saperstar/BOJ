0618142632aca8466185b3db39119ebd791dcf02
Saperstar <112953455+Saperstar@users.noreply.github.com>
2026-03-06 02:37:29 +0900

    [Bronze V] Title: X보다 작은 수, Time: 464 ms, Memory: 30800 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -0,0 +1,33 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.Scanner;
+import java.util.NoSuchElementException;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    
+	    int N = sc.nextInt();
+	    int num[] = new int[N];
+	    int X = sc.nextInt();
+	    
+
+	    for(int i=0; i<N; i++){
+	        int inp = sc.nextInt();
+	        num[i] = inp;
+	    }
+	    
+	    for(int i=0; i<N; i++){
+	        if(num[i] < X){
+	            System.out.print(num[i]+" ");
+	        }
+	    }
+		
+    }
+}
