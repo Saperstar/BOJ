6919036f948507bef31fde24ec47194079291ae4
Saperstar <112953455+Saperstar@users.noreply.github.com>
2026-03-06 02:37:51 +0900

    [Bronze V] Title: A+B - 5, Time: 184 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.Scanner;
+import java.util.NoSuchElementException;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    
+		while(true){
+		    int a = sc.nextInt();
+		    int b = sc.nextInt();
+		    if(a==b&&a==0){
+		        break;
+		    }
+		    else{
+		        System.out.println(a+b);
+		    }
+		}
+		
+    }
+}
