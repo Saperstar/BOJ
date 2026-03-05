02d80ce8d633adf0b537bf8e7342bfd51bf1a124
Saperstar <112953455+Saperstar@users.noreply.github.com>
2026-03-06 02:39:36 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main(){
+    int t,a,b;
+    scanf("%d",&t);
+    for(int i=0; i<t; i++){
+        scanf("%d %d",&a,&b);
+        printf("Case #%d: %d\n",i+1,a+b);
+    }
+    
+    return 0;
+}
