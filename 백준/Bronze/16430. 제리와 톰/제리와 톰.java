a1342482ca69cac46262ff1ceb24dcab2a553a58
Saperstar <112953455+Saperstar@users.noreply.github.com>
2026-03-06 02:48:50 +0900

    [Bronze V] Title: 제리와 톰, Time: 128 ms, Memory: 16476 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.java
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+import java.io.*;
+public class Main{
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String[] line = br.readLine().split(" ");
+		int ans = Integer.parseInt(line[1]) - Integer.parseInt(line[0]);
+		bw.write(ans + " " + line[1]);
+		bw.flush();
+		
+		
+	}
+}
