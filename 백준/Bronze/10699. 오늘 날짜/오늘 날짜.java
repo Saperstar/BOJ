e557e629d862cb6dae28956adc5a89d2ec29a538
Saperstar <112953455+Saperstar@users.noreply.github.com>
2026-03-06 02:13:34 +0900

    [Bronze V] Title: 오늘 날짜, Time: 156 ms, Memory: 16684 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.time.LocalDate;
+import java.time.ZoneId;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		LocalDate now = LocalDate.now(ZoneId.of("Asia/Seoul"));
+		
+		bw.write(now + "");
+		bw.flush();
+
+	}
+
+}
