c917769f4356ae1a47b71e98946fb0afa787fe7a
Saperstar <112953455+Saperstar@users.noreply.github.com>
2026-03-06 02:26:27 +0900

    [Bronze V] Title: Metronome, Time: 112 ms, Memory: 14500 KB -BaekjoonHub

---
+++ 백준/Bronze/27389. Metronome/Metronome.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		double n = Integer.parseInt(br.readLine());
+		double ans = n / 4;
+		
+		bw.write(String.format("%.2f", ans));
+		bw.flush();
+		
+		
+
+	}
+	
+
+}
