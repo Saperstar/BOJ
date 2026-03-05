93bb41635efe3807ead34047784e1567410b904e
Saperstar <112953455+Saperstar@users.noreply.github.com>
2026-03-06 02:18:46 +0900

    [Bronze V] Title: 큰 수 곱셈, Time: 5528 ms, Memory: 406900 KB -BaekjoonHub

---
+++ 백준/Bronze/13277. 큰 수 곱셈/큰 수 곱셈.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String[] line = br.readLine().split(" ");
+		BigInteger A = new BigInteger(line[0]);
+		BigInteger B = new BigInteger(line[1]);
+		bw.write(A.multiply(B) + "");
+		bw.flush();
+
+	}
+
+}
